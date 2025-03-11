03f8d4b2f10b8014107909c49c44132c130b2445
Karan <karanchaurasiya2003a@gmail.com>
2025-03-11 21:33:39 +0530

    Add files via upload

---
+++ dark-mode-toggle/script.js
@@ -0,0 +1,22 @@
+const inputEl = document.querySelector(".input");
+
+const bodyEl = document.querySelector("body");
+
+inputEl.checked = false;
+
+updateBody()
+
+function updateBody() {
+    if(inputEl.checked){
+        bodyEl.style.background = "black";
+    }else{
+        bodyEl.style.background = "white"
+    }
+
+}
+
+inputEl.addEventListener("input", ()=>{
+    updateBody()
+})
+
+
